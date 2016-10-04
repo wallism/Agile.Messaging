ff8c9ada8412ec8a3daa4076bf9d1fc2a854839b
Mark Wallis <mark.wallis@rockend.com.au>
2016-10-05 08:33:20 +1100

    config setup, cmd prj created

---
+++ Agile.MessageHub.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agile.MessageHub.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setting = ConfigurationManager.AppSettings["Setting1"];
+            System.Console.WriteLine(setting);
+
+            var conn = ConfigurationManager.ConnectionStrings["Logging"];
+            Console.WriteLine(conn);
+            System.Console.ReadLine();
+        }
+    }
+}
